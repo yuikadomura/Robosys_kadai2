1964812d22d9bcdd10ddaf76862313ded6663cf6
Yui Kadomura <yui314jumping@gmail.com>
2022-01-08 14:37:48 +0000

    add a new file

---
+++ count.py
@@ -0,0 +1,16 @@
+//BSD 3-Clause License
+//Copyright (c) 2021 Ryuichi Ueda. All rights reserved
+
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node('count')
+pub = rospy.Publisher('count_up', Int32, queue_size=1)
+rate = rospy.Rate(10)
+n = 0
+
+while not rospy.is_shutdown():
+    n = int(datetime.datetime.now())
+    pub.publish(n)
+    rate.sleep()
